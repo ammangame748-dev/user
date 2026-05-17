f0994ecdecf36d11343e771e76f6c1032fef5e2d
ammangame748-dev <ammangame748@gmail.com>
2026-05-18 02:16:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const CONFIG_FILE = path.join(__dirname, 'progress.json');
 
 const CLIENT_ID = process.env.CLIENT_ID || "1501846584961532004";
 const CLIENT_SECRET = process.env.CLIENT_SECRET || "lKyk-Mjv8FYAQMCXhPw0kd2A0-RoqX2W";
-const REDIRECT_URI = process.env.REDIRECT_URI || "https://YOUR-APP.onrender.com/auth/callback";
+const REDIRECT_URI = process.env.REDIRECT_URI || "https://zone-bot-xfli.onrender.com/callback";
 
 function loadConfig() {
     if (!fs.existsSync(CONFIG_FILE)) fs.writeFileSync(CONFIG_FILE, JSON.stringify({}));
